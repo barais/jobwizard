e9b15d476dfeffb12537b30a3a593c7422f7f48d
Carsten Bleek <bleek@cross-solution.de>
2022-03-31 15:57:46 +0200

    chore: langing page. Section Create job ads

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,8 +1,39 @@
 <template>
   <q-page padding align="center">
-    <h1>{{ $t('landing') }}</h1>
+    <h2>{{ $t('landing') }}</h2>
+
+    <div class="row q-my-xl">
+      <q-card class="col-6">
+        <q-card-section>
+          <q-list>
+            <q-item>
+              <q-item-section>
+                <q-item-label>
+                  {{ $t('create_job_postings') }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card-section>
+      </q-card>
+      <q-card class="col-6" flat>
+        <q-carousel
+          v-model="slide"
+          animated
+          arrows
+          control-color="primary"
+          navigation
+          infinite
+        >
+          <q-carousel-slide :name="1" img-src="../../public/images/screens/create_job-1.png" />
+          <q-carousel-slide :name="2" img-src="../../public/images/screens/create_job-2.png" />
+          <q-carousel-slide :name="3" img-src="../../public/images/screens/create_job-3.png" />
+        </q-carousel>
+      </q-card>
+    </div>
+
     <q-btn
-      size="10vh"
+      size="xl"
       color="secondary"
       rounded
       push
@@ -13,24 +44,33 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
   name: 'Landing',
+  setup()
+  {
+    return {
+      slide: ref(1)
+    };
+  }
+
 };
 </script>
 
 <i18n>
-  {
-    "en": {
-      "landing": "The requested page could not be found",
-      "go-home": "go to home page",
-    },
-    "de": {
-      "landing": "Kostenlos Anzeigen verwalten und schalten",
-      "file-not-found": "Die angeforderte Seite konnte nicht gefunden werden"
-    },
-    "fr": {
-      "landing": "vers la page d'accueil",
-      "file-not-found": "La page demandée n'a pas été trouvée"
-    }
+{
+  "en": {
+    "landing": "Manage and place ads free of charge",
+    "create_job_postings": "Create job advertisements"
+  },
+  "de": {
+    "landing": "Kostenlos Anzeigen verwalten und schalten",
+    "create_job_postings": "Stellenanzeigen erstellen"
+  },
+  "fr": {
+    "landing": "Gérer et diffuser des annonces gratuitement",
+    "create_job_postings": "Créer des annonces d'emploi"
   }
+}
 </i18n>
