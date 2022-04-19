9d31e9ffa91bf87e9341585dd4a29478294ea35a
Carsten Bleek <bleek@cross-solution.de>
2022-04-19 19:15:54 +0200

    remove hardcoded url

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -295,7 +295,7 @@ export default {
         };
       }
       this.isLoading = true;
-      axios.post('https://api.yawik.org/api/auth/local', data).then(response =>
+      axios.post(this.loginUrl, data).then(response =>
       {
         let token,
           user;
