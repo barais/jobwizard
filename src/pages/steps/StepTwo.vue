603ee882f124a76bc299144ec84e11fd35449c6c
Carsten Bleek <bleek@cross-solution.de>
2022-04-22 10:52:06 +0200

    fix cypress !56

--- src/pages/steps/StepTwo.vue
+++ src/pages/steps/StepTwo.vue
@@ -314,7 +314,7 @@ export default
   mounted()
   {
     this.setFocus();
-    if (this.organization.id != null)
+    if (this.organization && this.organization.id != null)
     {
       this.getOrg(this.organization.id);
     }
